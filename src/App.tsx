525c6fa8cc283e6c9cec004952e497b9ac435752
Raymond <raima220@gmail.com>
2022-01-08 12:43:14 +0200

    use fetch to aquire API data and display in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import logo from './logo.svg'
 import tauriCircles from './tauri.svg'
 import tauriWord from './wordmark.svg'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+      if (data.length !== 0) {
+          return
+      }
+
+    fetch("https://baconipsum.com/api/?type=meat-and-filler")
+        .then(res => res.json())
+        .then(d => {
+          setData(d)
+        })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,26 +26,9 @@ function App() {
           <img src={tauriCircles} className="App-logo rotate" alt="logo" />
           <img src={tauriWord} className="App-logo smaller" alt="logo" />
         </div>
-        <a
-          className="App-link"
-          href="https://tauri.studio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tauri
-        </a>
-        <img src={logo} className="App-logo rotate" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {data.map((v, i) => (
+            <div key={"bacon-" + i}>{v}</div>
+        ))}
       </header>
     </div>
   )
