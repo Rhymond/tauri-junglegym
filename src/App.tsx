bee0c8724c64e3b02d002bb29d971e72ab59e7fe
Raymond <raima220@gmail.com>
2022-01-09 21:43:38 +0200

    play multiple videos in the loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const videos = ["video/1.webm", "video/2.webm"];
+  const [index, setIndex] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <video
-          src={process.env.PUBLIC_URL + "video/1.webm"}
+          src={process.env.PUBLIC_URL + videos[index]}
           style={{
             height: "100vh",
           }}
+          onEnded={(v) => {
+            setIndex(index >= videos.length - 1 ? 0 : index + 1);
+          }}
           autoPlay
           muted
-          loop
-          playsInline
         />
       </header>
     </div>
