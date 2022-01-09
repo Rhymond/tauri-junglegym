07426718dc7f9594d75ec9b4143a51dbad0f1c11
Raymond <raima220@gmail.com>
2022-01-09 17:45:48 +0200

    prettier added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <video
-            src={process.env.PUBLIC_URL + 'video/1.webm'}
-            style={{
-                height: "100vh"
-            }}
-            autoPlay
-            muted
-            loop
-            playsInline
+          src={process.env.PUBLIC_URL + "video/1.webm"}
+          style={{
+            height: "100vh",
+          }}
+          autoPlay
+          muted
+          loop
+          playsInline
         />
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
